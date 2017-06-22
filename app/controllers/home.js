a30ed4de9ccfe900a43e895d946adcbaa008caa1
dss13 <ssumugan007@gmail.com>
2017-06-22 18:37:33 +0530

    added instructions and recent activities to dashboard landing pagee

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -305,7 +305,7 @@ router.post('/admin/adduser', adminLoginStatus, function(req, res) {
 	user.find({ email: req.body.email }, function( err, result){
 		if(result.length) {
 			res.render('admin/manageusers', { layout: "adminnew", messageerror: "User with email id : "+ req.body.email+" already exists !"});
-		} else {	
+		} else {
 			var newUser = new user()
 			newUser.email = req.body.email
 			newUser.password = req.body.password
@@ -345,7 +345,7 @@ router.get('/admin/getpayments', adminLoginStatus, function(req, res) {
 });
 
 router.get('/admin/updateStatus/id=:id&&value=:value', adminLoginStatus, function(req, res) {
-	
+
 	var objectId = mongoose.Types.ObjectId(req.params.id);
 	payment.updateOne({'_id': objectId},{ 'status': req.params.value}, function(err) {
 		if(err) {
@@ -356,6 +356,18 @@ router.get('/admin/updateStatus/id=:id&&value=:value', adminLoginStatus, functio
 		}
 	});
 });
+
+router.get('/getlatestteams', checkloginstate, function(req,res) {
+	team.find({ useremail: req.user.email}, null, {sort: {'_id': -1}, limit: 3}, function(err, result) {
+		res.json(result)
+	})
+})
+
+router.get('/getlatestpays', checkloginstate, function(req, res) {
+	payment.find({ email: req.user.email}, null, {sort: {'_id': -1}, limit: 3}, function(err, result){
+		res.json(result);
+	})
+})
 
 function adminLoginStatus(req, res, next) {
 
